17928f631104764cc6b04ab6c236c643b73f9a54
Denis Howard <Denis9266466eHoward@users.noreply.github.com>
2026-04-15 22:02:22 +0800

    expose listExports from export module index

--- src/export/index.js
+++ src/export/index.js
@@ -8,15 +8,18 @@
  * browser session snapshots outside of the local tabforge store.
  *
  * Usage:
- *   const { exportSnapshot, importSnapshot } = require('./src/export');
+ *   const { exportSnapshot, importSnapshot, listExports } = require('./src/export');
  *
  *   // Save a snapshot to disk as a shareable file
  *   exportSnapshot(snapshot, './exports/my-session.json');
  *
  *   // Load it back later or on another machine
  *   const snapshot = importSnapshot('./exports/my-session.json');
+ *
+ *   // List all exported snapshot files in a directory
+ *   const files = listExports('./exports');
  */
 
-const { exportSnapshot, importSnapshot } = require('./export');
+const { exportSnapshot, importSnapshot, listExports } = require('./export');
 
-module.exports = { exportSnapshot, importSnapshot };
+module.exports = { exportSnapshot, importSnapshot, listExports };
